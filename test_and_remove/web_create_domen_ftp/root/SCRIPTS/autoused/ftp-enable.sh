436359edfbe58327c6d2ea0b410b5129c54f3391
Alexandr <sanekmihailow@gmail.com>
2020-07-14 17:22:06 +0300

    Create ftp-enable.sh

---
+++ test_and_remove/web_create_domen_ftp/root/SCRIPTS/autoused/ftp-enable.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+enuser="$1"
+user_path="/etc/vsftpd/user_list"
+path_home="/home/vtigers/${enuser}/domain"
+check_ht="$(echo $(grep "    RewriteRule.*domain.com/pay.*$" ${path_home}/.htaccess))"
+check_user="$(echo $(grep "${enuser}" ${user_path}))"
+
+if [ -z "$enuser" ]; then
+     echo 'empty enable user'
+     exit 1
+fi
+
+if [[ "$check_user" ]];then
+    echo '1' > /dev/null; else
+    echo "$enuser" >> "$user_path"
+fi
+
+if [[ "$check_ht" ]]; then
+    rm "$path_home"/.htaccess
+    if [ -f "$path_home"/htaccessOld ]; then
+        mv "$path_home"/htaccessOld "$path_home"/.htaccess
+        chown "$enuser":"$enuser" "$path_home"/.htaccess
+    fi    
+fi
+exit 0
