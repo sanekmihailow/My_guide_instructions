6bf790880161f2162c731b6a1f21f484677b1c0e
Alexandr <sanekmihailow@gmail.com>
2020-07-17 16:51:07 +0300

    Update ftp-enable.sh

--- test_and_remove/web_create_domen_ftp/root/SCRIPTS/autoused/ftp-enable.sh
+++ test_and_remove/web_create_domen_ftp/root/SCRIPTS/autoused/ftp-enable.sh
@@ -2,8 +2,10 @@
 enuser="$1"
 user_path="/etc/vsftpd/user_list"
 path_home="/home/vtigers/${enuser}/domain"
+enpass="$(awk '(NR==26)' ${path_home}/config.inc.php |awk '{print $3}' |sed "s/^'//" |sed "s/';$//")"
 check_ht="$(echo $(grep "    RewriteRule.*domain.com/pay.*$" ${path_home}/.htaccess))"
 check_user="$(echo $(grep "${enuser}" ${user_path}))"
+mysqlroot="mysql --login-path=localRoot"
 
 if [ -z "$enuser" ]; then
      echo 'empty enable user'
@@ -16,10 +18,12 @@ if [[ "$check_user" ]];then
 fi
 
 if [[ "$check_ht" ]]; then
-    rm "$path_home"/.htaccess
+    echo '1' > /dev/null
     if [ -f "$path_home"/htaccessOld ]; then
-        mv "$path_home"/htaccessOld "$path_home"/.htaccess
+#M-        mv "$path_home"/htaccessOld "$path_home"/.htaccess
         chown "$enuser":"$enuser" "$path_home"/.htaccess
-    fi    
+    fi
+    ${mysqlroot} -e "GRANT ALL ON ${enuser}.* to '${enuser}'@'%' IDENTIFIED by '${enpass}';" && echo 'enabled'
+    ${mysqlroot} -e "flush privileges;"   
 fi
 exit 0
