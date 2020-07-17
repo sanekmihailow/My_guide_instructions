c052100b4fc2411a816ac4ee58c4c06397fc03b8
Alexandr <sanekmihailow@gmail.com>
2020-07-17 16:55:50 +0300

    Update ftp-disable.sh

--- test_and_remove/web_create_domen_ftp/root/SCRIPTS/autoused/ftp-disable.sh
+++ test_and_remove/web_create_domen_ftp/root/SCRIPTS/autoused/ftp-disable.sh
@@ -2,17 +2,20 @@
 template="/root/SCRIPTS/template/disable-htaccess"
 disuser="$1"
 path_home="/home/vtigers/${disuser}/domain"
+mysqlroot="mysql --login-path=localRoot"
 
 if [ -z "$disuser" ]; then
     echo 'empty disable user'
     exit 1
 fi    
 
-grep -n "$disuser" /etc/vsftpd/user_list   |awk -F : '{print $1}' > /tmp/results/ftp-disable
+    grep -n "$disuser" /etc/vsftpd/user_list   |awk -F : '{print $1}' > /tmp/results/ftp-disable
 dis=$(echo $(/bin/cat /tmp/results/ftp-disable))
-sed -i "${dis}d" /etc/vsftpd/user_list
-mv "$path_home"/.htaccess "$path_home"/htaccessOld
-chown "$disuser":"$disuser" "$path_home"/htaccessOld
-cp "$template" "$path_home"/.htaccess
-sed -i "s/servweb/${disuser}/g" "$path_home"/.htaccess
+    sed -i "${dis}d" /etc/vsftpd/user_list
+    mv "$path_home"/.htaccess "$path_home"/htaccessOld
+#M-    chown "$disuser":"$disuser" "$path_home"/htaccessOld
+    cp "$template" "$path_home"/.htaccess
+    sed -i "s/servweb/${disuser}/g" "$path_home"/.htaccess
+    ${mysqlroot} -e "DROP USER '${disuser}'@'%';"
+    ${mysqlroot} -e "flush privileges;"
 exit 0
