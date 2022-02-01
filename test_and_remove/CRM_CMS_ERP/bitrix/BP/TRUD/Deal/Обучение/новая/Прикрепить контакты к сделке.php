cfa07cef009f7d34175e221813d334fbb40d9e5a
Alexandr <sanekmihailow@gmail.com>
2022-02-01 11:14:49 +0300

    Update Прикрепить контакты к сделке.php

--- test_and_remove/CRM_CMS_ERP/bitrix/BP/TRUD/Deal/Обучение/новая/Прикрепить контакты к сделке.php
+++ test_and_remove/CRM_CMS_ERP/bitrix/BP/TRUD/Deal/Обучение/новая/Прикрепить контакты к сделке.php
@@ -1,1 +1,12 @@
+<?
+$rootActivity = $this->GetRootActivity();
+use \Bitrix\Crm;
+global $USER_FIELD_MANAGER;
 
+//#C- Const
+$deal_id = {{ID}};
+$ContactIds = "{=Variable:Contacts_id}";
+//#-----
+
+$arContactIds = explode(" ", $ContactIds);
+$rs = \Bitrix\Crm\Binding\DealContactTable::bindContactIDs($deal_id,$arContactIds);
