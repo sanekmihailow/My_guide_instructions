cd97e4387ee838a280f8e2c3be5d311822da1584
Alexandr <sanekmihailow@gmail.com>
2022-02-01 12:41:11 +0300

    Create оставить товары Обучение.php

---
+++ test_and_remove/CRM_CMS_ERP/bitrix/BP/TRUD/Deal/-bp/Дублирование Новой Сделки/оставить товары Обучение.php
@@ -0,0 +1,66 @@
+<?
+$rootActivity = $this->GetRootActivity();
+use \Bitrix\Crm;
+//--const
+$dealId = {{ID}};
+$NumCatalog = 301;
+$NumCatalog1 = 309;
+$NumCatalog2 = 310;
+$NumCatalog3 = 311;
+//--
+
+$arProducts = CCrmProductRow::LoadRows(D, $dealId);
+$count_products = count($arProducts);
+
+if (empty($count_products)) {
+    $error = 'NoTovar';
+    $rootActivity->SetVariable("ProductError",$error);
+} 
+else {
+	$numcount = "$count_products" - 1;
+	for ($i = 0; $i <= $numcount; $i++) {
+		$Idproducts[] = $arProducts["$i"]['PRODUCT_ID'];
+        $ID[] = $arProducts["$i"]['ID'];
+	}
+	foreach ($Idproducts as $id) {
+		$arCatalog = CCatalogProduct::GetByIDEx($id);
+		$arSectionCatalog[] = $arCatalog['IBLOCK_SECTION_ID'];
+	}
+
+    $arraysAll = ['IDs'=>$ID, 'Products'=>$Idproducts, 'Catalogs'=>$arSectionCatalog];
+    
+
+    for ($i = 0; $i <= $numcount; $i++) {
+        $pishem = 'YES';
+        $catalogid = $arraysAll['Catalogs'][$i];
+        $product_basket_id = $arraysAll['IDs'][$i];
+
+        if ($catalogid == "$NumCatalog" || $catalogid == "$NumCatalog1" || $catalogid == "$NumCatalog2" || $catalogid == "$NumCatalog3"){
+        } else {
+            $pishem = 'NO';
+        }
+
+        if ($pishem == 'YES'){
+            $change_products[] = $product_basket_id;
+        }
+    }
+
+    $count_products = count($change_products);
+    if (empty($count_products)) {
+        $er = 'NoTovar';
+    } else { 
+        foreach ($change_products as $id) {
+            $product_detail = CCrmProductRow::GetByID($id);
+            $Product_id = $product_detail['PRODUCT_ID'];
+            $Quanity = $product_detail['QUANTITY'];
+            $Price = $product_detail['PRICE'];
+            $product_rows[] = [
+                            'PRODUCT_ID' => $Product_id,
+                            'QUANTITY' => $Quanity,
+                            'PRICE' => $Price
+            ];
+        }
+        $res = CCrmProductRow::SaveRows('D', $dealId, $product_rows);
+    }
+
+}
