04abf324481c2d4d50c969afa3df29392e1ea3cf
Alexandr <sanekmihailow@gmail.com>
2022-02-17 20:49:18 +0300

    Update создание ЛК клиента.php

--- test_and_remove/CRM_CMS_ERP/bitrix/BP/TRUD/Deal/Обучение/ЛК/создание ЛК клиента.php
+++ test_and_remove/CRM_CMS_ERP/bitrix/BP/TRUD/Deal/Обучение/ЛК/создание ЛК клиента.php
@@ -7,6 +7,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/local/MyFunctions/IncludeFunctions.ph
 //#C- Const
 $deal_id = {{ID}};
 $numerator = {=Variable:Numerator};
+$backnumerator = $numerator;
 $error = '';
 $user_names = '';
 //#-----
@@ -39,6 +40,7 @@ if (empty($Ar_learnContact)) {
         $mail = $resmail['VALUE'];
     //#C----- 
 
+    //#C - str_replace("что","чем",$где);
         $name = $Ar_get['NAME'];
         $name = trim($name, " ");
         $lastname = $Ar_get['LAST_NAME'];
@@ -51,13 +53,15 @@ if (empty($Ar_learnContact)) {
         } else {
             $username = $get_username;
         }
+            $chars = ["'", ',' , 'ʹ'];
+            $username = str_replace($chars, '', $username);
         $password = random_password(22);
 
     //#C - check on exist email,firstname,lastname
         $Ar_user_email = rest_get_user_moodle('email',"$mail");
         $exist = sizeof($Ar_user_email['users']);
 
-        if (empty($mail) || $exist == '0') {
+        if (empty($mail) || $exist == 0) {
             $email = $username . "@trud-rf.ru";
         } else {
             $email = $mail;
@@ -74,16 +78,33 @@ if (empty($Ar_learnContact)) {
 		$create_user = sizeof($Ar_user['users']);
         $create_email = sizeof($Ar_email['users']);
         
-		if ($create_user == '0' && $create_email == '0') {
-            $new_user_id = rest_create_user_moodle($username, $password, $name, $lastname, $email);
-            $id_num = $new_user_id[0]['id'];
-            $id_num = (int)$id_num;
-			$update_learn_id = $USER_FIELD_MANAGER->Update('CRM_CONTACT', $id, array("UF_CRM_CONTACT_ID_USER_LEARN" => $numerator));
-            $update_learn_id = $USER_FIELD_MANAGER->Update('CRM_CONTACT', $id, array("UF_CRM_CONTACT_USERNAME_LEARN" => $username));
-            $update_learn_id = $USER_FIELD_MANAGER->Update('CRM_CONTACT', $id, array("UF_CRM_CONTACT_PASSWORD_LEARN" => $password));
-            $numerator = $id_num + 1;
-            $user_names = $usernames .= ", $username";
-            $ar_Contactids[] = $id; 
+		if ($create_user == 0 && $create_email == 0) {
+            if ($numerator != 0 && $numerator != 1) {
+                //$backnumerator = $numerator;
+                $new_user_id = rest_create_user_moodle($username, $password, $name, $lastname, $email, $deal_id);
+                $new_exist = sizeof($new_user_id[0]['id']);
+                if ($new_exist !== 0) {
+                    $backnumerator = $numerator;
+                    $id_num = $new_user_id[0]['id'];
+                    $id_num = (int)$id_num;
+                    $update_learn_id = $USER_FIELD_MANAGER->Update('CRM_CONTACT', $id, array("UF_CRM_CONTACT_ID_USER_LEARN" => $numerator));
+                    $update_learn_id = $USER_FIELD_MANAGER->Update('CRM_CONTACT', $id, array("UF_CRM_CONTACT_USERNAME_LEARN" => $username));
+                    $update_learn_id = $USER_FIELD_MANAGER->Update('CRM_CONTACT', $id, array("UF_CRM_CONTACT_PASSWORD_LEARN" => $password));
+                    $numerator = $id_num + 1;
+                    $user_names = $usernames .= ", $username";
+                    $ar_Contactids[] = $id;
+                    $Ar_emails[] = $email;
+                } else {
+                    $error = $error .= "ошибка - пользователь - $username in contact_id:$id не был создан";
+                }  
+            } else {
+                $check_created_user = rest_get_user_moodle('id',"$backnumerator");
+                $exist_created_user = sizeof($check_created_user['users']);
+                if ($exist_created_user !== 0) {
+                    $numerator = $backnumerator + 1;
+                }
+                $error = $error .= "ошибка - пользователь - $username in contact_id:$id присвоен не тот id\n";
+            }
 		} else {
 			$error = $error .= "пользователь - $username in contact_id:$id уже существует, либо пользователь с таким же email - \"$email\" в moodle существует; \n";
 		}     
@@ -96,3 +117,4 @@ $rootActivity->SetVariable("Error",$error);
 $rootActivity->SetVariable("Numerator",$numerator);
 $rootActivity->SetVariable("Usersnames",$user_names);
 $rootActivity->SetVariable("Ids_contact",$ar_Contactids);
+$rootActivity->SetVariable("Emails",$Ar_emails);
