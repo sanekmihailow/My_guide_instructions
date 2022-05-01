d0c916f1182e8d97d3ab3b63539fdbb5c4f272b2
Alexandr <sanekmihailow@gmail.com>
2022-05-01 13:06:28 +0300

    Update Bitrix_RestFunctions.php

--- test_and_remove/CRM_CMS_ERP/bitrix/MyFunctions/PHP/Bitrix_RestFunctions.php
+++ test_and_remove/CRM_CMS_ERP/bitrix/MyFunctions/PHP/Bitrix_RestFunctions.php
@@ -1,48 +1,60 @@
 <?
-function getInvoice($method, $id) {
+function getEnity($method, $id) {
     $http = 'https:/example.com/rest/';
-    $user = '10002131';
-    $token = 'аяаяаяаяаяаяаяаяыя';
+    $user = '1321235';
+
+    if ($method == 'crm.invoice.get') {
+        $token = 'аяаяаяаяаяаяаяаяыя';
+    } else if ($method == 'crm.deal.get') {
+        $token = 'аяаяаяаяeqweqweqweq';
+    }
+
     $url_request = $http . $user . '/' . $token . '/' . "$method" . '?' . 'id=' . "$id";
 
     $curl = curl_init();
     curl_setopt_array($curl, [
-        CURLOPT_URL => "$url_request",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_URL => "$url_request",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
     ]);
 
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return json_decode($response, true);
+      $response = curl_exec($curl);
+      curl_close($curl);
+      return json_decode($response, true);
 }
 
-function invoiceUpdate ($method, $id, $fields) {
+function enityUpdate ($method, $id, $fields) {
     $http = 'https:/example.com/rest/';
-    $user = '10002131';
-    $token = 'аяаяаяаяаяаяаяаяыя';
+    $user = '1321235';
+
+    if ($method == 'crm.invoice.update') {
+        $token = 'аяаяаяаяая31231231аяаяаяыя';
+    } else if ($method == 'crm.deal.update') {
+        $token = 'ewqeqweqeqsczcxcz';
+    }
+
     $url_request = $http . $user . '/' . $token . '/' . "$method" . '?' . 'id=' . "$id" . '&' . "$fields";
-    $curl = curl_init();
 
-    curl_setopt_array($curl, [
-        CURLOPT_URL => "$url_request",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-    ]);
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+                CURLOPT_URL => "$url_request",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+        ]);
 
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return json_decode($response, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response, true);
 }
 
 function postToChat($dialog, $message, $system_x) {
