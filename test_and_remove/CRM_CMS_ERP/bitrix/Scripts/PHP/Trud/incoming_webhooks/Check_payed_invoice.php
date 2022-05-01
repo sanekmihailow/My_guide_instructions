f86200544fe67152cf5d71401a46c1437717e68c
Alexandr <sanekmihailow@gmail.com>
2022-05-01 13:09:17 +0300

    Update Check_payed_invoice.php

--- test_and_remove/CRM_CMS_ERP/bitrix/Scripts/PHP/Trud/incoming_webhooks/Check_payed_invoice.php
+++ test_and_remove/CRM_CMS_ERP/bitrix/Scripts/PHP/Trud/incoming_webhooks/Check_payed_invoice.php
@@ -5,9 +5,7 @@ $request = $_REQUEST;
 //writeToLog($request, 'Invoice_incomming');
 
 $invoice_id = $request['data']['FIELDS']['ID'];
-$res = getInvoice('crm.invoice.get', $invoice_id);
-  //$arr = json_decode($res,true);
-  //$arInvoice = $arr['result'];
+$res = getEnity('crm.invoice.get', $invoice_id);
 $arInvoice = $res['result'];
 $status = $arInvoice['STATUS_ID'];
 $user_changed = $arInvoice['EMP_STATUS_ID'];
@@ -17,11 +15,10 @@ $user_assigned = $arInvoice['RESPONSIBLE_ID'];
 $InvalidStatus = 'fields%5BSTATUS_ID%5D=N';
 $num_lma = '62'; $num_1 = '1'; $num_change_1c = '158'; $mae='100'; $boss_lab='118';
 
-//echo $user_payed;
 if ($status == 'P' || $status == 'D') {
 
     if ($user_payed !== $num_lma && $user_payed !== $num_1 && $user_payed !== $mae) {
-        invoiceUpdate('crm.invoice.update', $invoice_id, $InvalidStatus);
+        enityUpdate('crm.invoice.update', $invoice_id, $InvalidStatus);
         $message[] = "Зафиксирована попытка самовольно завершения счета, либо изменение уже оплаченного счета. Счет id:$invoice_id";
         $converted_message = http_build_query($message);
         $converted_message = substr($converted_message, 2);
