1f571ed2f0ef700688e99d0f1b36342c5579c341
Alexandr <sanekmihailow@gmail.com>
2022-11-07 11:51:46 +0300

    fix php notice
    PHP Notice:  Trying to access array offset on value of type null in line 17

--- test_and_remove/CRM_CMS_ERP/bitrix/Scripts/PHP/Trud/incoming_webhooks/activity_create.php
+++ test_and_remove/CRM_CMS_ERP/bitrix/Scripts/PHP/Trud/incoming_webhooks/activity_create.php
@@ -6,7 +6,6 @@ $request = $_REQUEST;
 $activity_id = $request['data']['FIELDS']['ID'];
 $res = getEnity('crm.activity.get', $activity_id);
 $arActivty = $res['result'];
-//$id = $arActivty['ID']; // id activity
 $enity_id = $arActivty['OWNER_ID'];
 $enity_type = $arActivty['OWNER_TYPE_ID'];
 $activity_type = $arActivty['TYPE_ID'];
@@ -15,24 +14,19 @@ $provider_type = $arActivty['PROVIDER_TYPE_ID'];
 $activity_subject = $arActivty['SUBJECT'];
 $activity_completed = $arActivty['COMPLETED'];
 $activity_assigned = $arActivty['RESPONSIBLE_ID'];
-$activity_settings = $arActivty['SETTINGS'];
-$activity_missed = '';
-if ($activity_settings) {
-    $activity_missed = $arActivty['SETTINGS'['MISSED_CALL']];
-}
-
-$activity_missed = $arActivty['SETTINGS'['MISSED_CALL']];
+    $activity_missed = (isset($arActivty['SETTINGS']['MISSED_CALL']) 
+                                ? $arActivty['SETTINGS']['MISSED_CALL'] : '');
 $subject = substr($activity_subject,0,21);
 $pattern = "/^Входящий/";
 
 if (preg_match("$pattern", $subject)) {
     if ($provider_type == 'CALL') {
         if ($activity_type == '2') {
-                writeToLog($assigned_deal, "activity:$activity_assigned");
+           // writeToLog($assigned_deal, "activity:$activity_assigned");
             if ($activity_completed == 'N') {
                 if ($activity_missed) { // if bool true
                     $arDealId = [];
-                    $resBindings = activityGetBindings('crm.activity.binding.list', $ar_id); //get all bindings activity
+                    $resBindings = activityGetBindings('crm.activity.binding.list', $activity_id); //get all bindings activity
                     $arBindings = $resBindings['result'];
                     foreach ($arBindings as ["entityTypeId" => $entityTypeId, "entityId" => $entityId]) {
                         if ($entityTypeId == '2') { // check deal bindings
@@ -47,7 +41,7 @@ if (preg_match("$pattern", $subject)) {
                             $arDeal = $resD['result'];
                             $assigned_deal = $arDeal['ASSIGNED_BY_ID'];
                             if ($assigned_deal == $activity_assigned) { // check equals assigned activity and assigned deal
-                                writeToLog($assigned_deal, "activity:$activity_assigned");
+                              //  writeToLog($assigned_deal, "activity:$activity_assigned");
                             } else {
                                 $typeId = '2';
                                 activityUpdateBindings('crm.activity.binding.delete', $activity_id, $typeId, $deal_id); //delete wrong bindings in activity
@@ -60,3 +54,4 @@ if (preg_match("$pattern", $subject)) {
         }
     }
 }
+
